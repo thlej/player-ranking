d72fc75b32df33d17ef0b33fb2e0442e6e53cd2c
t.lejeune <tlejeune86165@gmail.com>
2021-05-18 19:56:08 +0200

    refactor: optimize imports

--- src/main/kotlin/fr/tle/infrastructure/configuration/Serialization.kt
+++ src/main/kotlin/fr/tle/infrastructure/configuration/Serialization.kt
@@ -1,10 +1,10 @@
 package fr.tle.infrastructure.configuration
 
-import io.ktor.serialization.*
-import io.ktor.features.*
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
