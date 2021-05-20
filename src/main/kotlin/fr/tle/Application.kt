12cd09bba22fb01dc8b971152bc9dcb45389e67d
t.lejeune <tlejeune86165@gmail.com>
2021-05-20 14:12:58 +0200

    feature: install CallLogging + StatusPages

--- src/main/kotlin/fr/tle/Application.kt
+++ src/main/kotlin/fr/tle/Application.kt
@@ -9,11 +9,16 @@ import fr.tle.infrastructure.configuration.configureSerialization
 import fr.tle.infrastructure.persistence.mongo.MongoPlayerRepository
 import fr.tle.infrastructure.persistence.mongo.PlayerDocument
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.koin.ktor.ext.Koin
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.getCollection
+import org.slf4j.event.Level
+import java.lang.IllegalArgumentException
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
@@ -31,6 +36,19 @@ fun Application.module(testing: Boolean = false, koinModules: List<Module> = lis
     install(Koin) {
 //        slf4jLogger() FIXME crash startup...
         modules(koinModules)
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+    }
+
+    install(StatusPages) {
+        exception<IllegalArgumentException> { e ->
+            call.respondText(
+                text = e.localizedMessage,
+                status = HttpStatusCode.BadRequest
+            )
+        }
     }
 }
 
